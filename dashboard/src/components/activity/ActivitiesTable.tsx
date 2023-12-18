31f7d47b9dbb546e0e63c959b9b550a9d942bb4a
fuzoh <info@bastiennicoud.ch>
2023-12-18 11:31:02 +0100

    add: loop to display event registrations

--- dashboard/src/components/activity/ActivitiesTable.tsx
+++ dashboard/src/components/activity/ActivitiesTable.tsx
@@ -1,24 +1,12 @@
-import { type Component } from "solid-js";
+import { type Component, For } from "solid-js";
 import { SeparatorRow } from "~/components/activity/table/SeparatorRow";
 import { HeadingRow } from "~/components/activity/table/HeadingRow";
 import { DataRow } from "~/components/activity/table/DataRow";
 import { type EventRegistration } from "~/dto/EventRegistration";
 
 interface Props {
-  activities: any[];
+  activities: EventRegistration[];
 }
-
-const event = {
-  id: "XXXX2941J6SY",
-  detailsLink: "https://toto.tutu.com/events/12345",
-  name: "My super event",
-  eventId: 12345,
-  customer: "Toto Tutu",
-  customerZip: 1234,
-  price: 10.00,
-  currency: "CHF",
-  type: "online_presale"
-} satisfies EventRegistration;
 
 export const ActivitiesTable: Component<Props> = (props) => {
   return (
@@ -31,15 +19,13 @@ export const ActivitiesTable: Component<Props> = (props) => {
         <time dateTime="2023-03-22">Today</time>
       </SeparatorRow>
 
-      <DataRow data={event} />
-      <DataRow data={event} />
-      <DataRow data={event} />
+      <For each={props.activities}>
+        {(item) => <DataRow data={item} />}
+      </For>
 
       <SeparatorRow>
         <time dateTime="2023-03-21">Yesterday</time>
       </SeparatorRow>
-
-      <DataRow data={event} />
       </tbody>
     </table>
   );
--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -4,6 +4,20 @@ import { StatsContainer } from "~/components/stats/StatsContainer";
 import { StatsElement } from "~/components/stats/StatsElement";
 import { ActivitiesContainer } from "~/components/activity/ActivitiesContainer";
 import { ActivitiesTable } from "~/components/activity/ActivitiesTable";
+import { type EventRegistration } from "~/dto/EventRegistration";
+
+const events = [{
+  id: "XXXX2941J6SY",
+  detailsLink: "https://toto.tutu.com/events/12345",
+  name: "My super event",
+  eventId: 12345,
+  customer: "Toto Tutu",
+  customerZip: 1234,
+  price: 10.00,
+  currency: "CHF",
+  type: "online_presale"
+}
+] satisfies EventRegistration[];
 
 export const Home: Component = () => {
   const links = [
@@ -35,7 +49,7 @@ export const Home: Component = () => {
       <div class="space-y-16 py-16 xl:space-y-20">
         {/* <!-- Recent activity table --> */}
         <ActivitiesContainer title="Derniers billets vendus">
-          <ActivitiesTable />
+          <ActivitiesTable activities={events} />
         </ActivitiesContainer>
 
       </div>
