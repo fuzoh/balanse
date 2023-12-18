7b1e6d6e97d4293a732e5174a74ee9a47ccd4425
fuzoh <info@bastiennicoud.ch>
2023-12-18 09:36:12 +0100

    add: stats section

---
+++ dashboard/src/components/stats/Progress.tsx
@@ -0,0 +1,27 @@
+import { type Component } from "solid-js";
+
+interface Props {
+  value: number;
+  unit: string;
+}
+
+export const Progress: Component<Props> = (props) => {
+  const prefix = (val: number): string => {
+    if (val > 0) return "+";
+    if (val < 0) return "-";
+    return "";
+  }
+
+  return (
+    <dd
+      class="text-xs font-medium"
+      classList={{
+        "text-gray-700": props.value === 0,
+        "text-green-600": props.value > 0,
+        "text-rose-600": props.value < 0
+      }}
+    >
+      {prefix(props.value)}{Math.abs(props.value)}{props.unit}
+    </dd>
+  )
+}
---
+++ dashboard/src/components/stats/StatsElement.tsx
@@ -0,0 +1,21 @@
+import { type Component } from "solid-js";
+import { Progress } from "~/components/stats/Progress";
+
+interface Props {
+  title: string;
+  value: number;
+  progress: number;
+}
+export const StatsElement: Component<Props> = (props) => {
+  return (
+    <div
+      class="flex items-baseline flex-wrap justify-between gap-y-2 gap-x-4 border-t border-gray-900/5 px-4 py-10 sm:px-6 lg:border-t-0 xl:px-8 sm:border-l first:border-l-0"
+    >
+      <dt class="text-sm font-medium leading-6 text-gray-500">{props.title}</dt>
+      <Progress value={props.progress} unit="%" />
+      <dd
+        class="w-full flex-none text-3xl font-medium leading-10 tracking-tight text-gray-900">{props.value}
+      </dd>
+    </div>
+  )
+}
