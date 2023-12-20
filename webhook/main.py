41eb3c6efe534229a008b0b1e3ac19b1bcaa602c
fuzoh <info@bastiennicoud.ch>
2023-12-20 19:41:52 +0100

    add: produce message on kafka for new events

--- webhook/main.py
+++ webhook/main.py
@@ -1,8 +1,10 @@
 import datetime
 import hmac
+import json
 import uuid
 
 import redis
+from confluent_kafka import Producer
 from flask import Flask, request, make_response, Response
 from redis.commands.json.path import Path
 
@@ -12,6 +14,12 @@ secret = 'my-super-secret'
 
 # Establish redis connection
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# Kafka connection
+kafka_producer = Producer({
+    'bootstrap.servers': 'localhost:9092',
+    'client.id': 'flask-petzi-webhook'
+})
 
 
 @app.post("/petzi-webhook")
@@ -24,7 +32,8 @@ def hello_world() -> Response:
     assert verify_message_age(signature_parts['t'], 10)
 
     event = request.json
-    save_event_to_database(event)
+    ticket_id = save_event_to_database(event)
+    notify_event_to_message_broker(ticket_id, event)
 
     response = make_response()
     response.status_code = 200
@@ -49,9 +58,29 @@ def verify_message_age(message_age: str, tolerance: int) -> bool:
     return time_delta.total_seconds() <= tolerance
 
 
-def save_event_to_database(event: dict) -> None:
+def save_event_to_database(event: dict) -> uuid.UUID:
     """
     Save the event to the database
     :param event:
     """
-    r.json().set(f'petzi-ticket:{uuid.uuid4()}', Path.root_path(), event)
+    event_id = uuid.uuid4()
+    r.json().set(f'petzi-ticket:{id}', Path.root_path(), event)
+    return event_id
+
+
+def notify_event_to_message_broker(event_id: uuid.UUID, event: dict) -> None:
+    """
+    Notify the event to the message broker
+    :param event_id:
+    :param event:
+    """
+    message_payload = {
+        'id': str(event_id),
+        'redis_key': 'petzi-ticket',
+        'buyer': f"{event['details']['buyer']['firstName']} {event['details']['buyer']['lastName']}",
+        'buyer_npa': event['details']['buyer']['postcode'],
+        'petzi_number': event['details']['ticket']['number'],
+        'name': event['details']['ticket']['event'],
+        'price': event['details']['ticket']['price']['amount']
+    }
+    kafka_producer.produce('tickets', json.dumps(message_payload))
